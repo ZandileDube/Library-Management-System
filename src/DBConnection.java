d5fb4c0908e781beb05cb779a1816109f696fa99
Author Name <zandiledube4th@gmail.com>
2023-01-13 21:19:13 +0200

    Initial commit:Register page implementation, including form validation and user account creation

---
+++ src/DBConnection.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+public class DBConnection {
+    static Connection con =null;
+
+
+    public static  Connection getConnection(){
+        try{
+
+            //establish connection
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/library_ms", "root", "");
+
+
+
+
+        }catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
+            return con;
+    }
+
+}
---
+++ src/RegistrationForm.java
@@ -0,0 +1,132 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+public class RegistrationForm extends JDialog {
+    private JTextField txtEmail;
+    private JTextField txtPhone;
+    private JTextField txtAddress;
+    private JPasswordField pwdtxtPassword;
+    private JButton btnCancel;
+    private JTextField txtName;
+    private JButton btnRegister;
+    private JLabel lblAddress;
+    private JPanel registerPanel;
+    private JPasswordField pwdtxtConfirmpassword;
+
+    public RegistrationForm(JFrame parent) {
+        super(parent);
+        setTitle("Create a new account");
+        setContentPane(registerPanel);
+        setMinimumSize(new Dimension( 450,474));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+
+        btnRegister.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                registerUser();
+
+            }
+        });
+        btnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+
+        setVisible(true);
+    }
+
+    private void registerUser()   {
+         String name = txtName.getText();
+         String email= txtEmail.getText();
+         String phone = txtPhone.getText();
+         String address = txtAddress.getText();
+         String password = String.valueOf(pwdtxtPassword.getPassword());
+         String Confirmpassword = String.valueOf(pwdtxtConfirmpassword.getPassword());
+
+
+        if(name.isEmpty() || email.isEmpty() || phone.isEmpty() || address.isEmpty() || password.isEmpty() || Confirmpassword.isEmpty()){
+            JOptionPane.showMessageDialog(this,
+                    "Please fill all fields",
+                    "Try again" ,
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+
+        }
+
+        if(!password.equals(Confirmpassword)){
+            JOptionPane.showMessageDialog(this,
+                    "Password does not match",
+                    "Try again" ,
+                    JOptionPane.ERROR_MESSAGE);
+
+return;
+        }
+        user = addNewUserToDB(name,email,phone,address,password);
+        if (user != null) {
+            dispose();
+        }
+        else {
+             JOptionPane.showMessageDialog(this,"Failed to register new user","Try again", JOptionPane.ERROR_MESSAGE);
+
+        }
+
+    }
+    public User user;
+    private User addNewUserToDB(String name, String email, String phone, String address, String password) {
+        User user = null;
+
+        try{
+            Connection conn = DBConnection.getConnection();
+            Statement stat = conn.createStatement();
+            String sql = "INSERT INTO users (name, email, phone, address, password)" + "VALUES(?,?,?,?,?)";
+
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1,name);
+            preparedStatement.setString(2,email);
+            preparedStatement.setString(3, phone);
+            preparedStatement.setString(4, address);
+            preparedStatement.setString(5, password);
+
+            int addedRows = preparedStatement.executeUpdate();
+            if (addedRows > 0) {
+                user = new User();
+                user.name = name;
+                user.email = email;
+                user.phone = phone;
+                user.address = address;
+                user.password = password;
+            }
+
+            stat.close();
+            conn.close();
+
+        }catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
+
+        return user;
+    }
+    public static void main(String[] args) {
+        RegistrationForm myForm = new RegistrationForm(null);
+        User user = myForm.user;
+        if (user != null) {
+            System.out.println("Successful registration of:" + user.name);
+        }else {
+            System.out.println("Registration canceled");
+        }
+    }
+
+
+}
