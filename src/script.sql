12ea07ebe4cf3bd7a37a9016e0cd1717ac74a85f
Zandile Dube <zandiledube4th@gmail.com>
2023-03-22 18:30:46 +0200

    hello tired

--- src/script.sql
+++ src/script.sql
@@ -5,3 +5,4 @@ CREATE TABLE User ( userID INT NOT NULL AUTO_INCREMENT, name varchar(30), surnam
 CREATE TABLE Status ( statusID int NOT NULL AUTO_INCREMENT,notBorrowed varchar(),   no_reserved_books int, no_lost_books int, fine_amount int, PRIMARY KEY(accountID), FOREIGN KEY (loanID) REFERENCES borrowBook(loanID));
 
 CREATE TABLE logs (logID int NOT NULL AUTO_INCREMENT, userID int ,bookID int, dateborrowed date, dateReturned date, PRIMARY KEY (logID), FOREIGN KEY (userID) REFERENCES user(userID), FOREIGN KEY (bookID) REFERENCES books(bookID));
+
